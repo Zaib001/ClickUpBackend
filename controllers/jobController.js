c2a88410f052f20d646ab3e56e2c9e46b2505514
TalhaZaheer1 <tzaheer72@gmail.com>
2025-05-06 00:10:38 -0700

    updates

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -19,3 +19,29 @@ export const getAllJobs = async (req, res, next) => {
     next(error);
   }
 };
+export const resetJobs = async (req, res, next) => {
+  try {
+    await Job.deleteMany({});
+    res.status(200).json({ success: true, message: "All jobs have been reset." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateJob = async (req, res, next) => {
+  try {
+    const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedJob) {
+      return res.status(404).json({ success: false, message: "Job not found." });
+    }
+
+    res.status(200).json({ success: true, data: updatedJob });
+  } catch (error) {
+    next(error);
+  }
+};
+
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { createJob, getAllJobs } from "../controllers/jobController.js";
+import {
+  createJob,
+  getAllJobs,
+  resetJobs,
+  updateJob,
+} from "../controllers/jobController.js";
 
 const router = express.Router();
 
-router.route("/jobs").post(createJob).get(getAllJobs);
+router.route("/jobs")
+  .post(createJob)
+  .get(getAllJobs);
+
+router.route("/jobs/reset")
+  .delete(resetJobs);
+
+router.route("/jobs/:id")
+  .put(updateJob);
 
 export default router;
